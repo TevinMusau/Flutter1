e34e988679c3f650f536dab46ba3457cb1ba6d35
Tevin <tevinmutua@gmail.com>
2026-08-08 17:50:31 +0300

    Initial Commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Loading Screen"),
+    );
+  }
+}
